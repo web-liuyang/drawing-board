9fd1f8f2a5912b82ee7bb14931499e19fc165df3
web-liuyang <984584014@qq.com>
2024-04-29 18:33:58 +0800

    Fix: improve GraphController

--- src/lib/drawing-board/graph-controller.ts
+++ src/lib/drawing-board/graph-controller.ts
@@ -1,6 +1,7 @@
-import { Graph } from "../graph/graph";
+import type { Graph } from "../graph";
+import { ChangeNotifier } from "../notifier";
 
-export class Controller {
+export class GraphController extends ChangeNotifier {
   private readonly _graphs: Graph[] = [];
 
   public get graphs(): Graph[] {
@@ -9,17 +10,24 @@ export class Controller {
 
   public addGraph(graph: Graph): void {
     this._graphs.push(graph);
+    this.notifyListeners();
+  }
+
+  public findGraph(id: Graph["id"]): Graph | undefined {
+    return this._graphs.find(g => g.id === id);
   }
 
   public updateGraph(id: Graph["id"], newGraph: Graph): void {
     const index = this._graphs.findIndex(g => g.id === id);
     if (index === -1) return;
     this._graphs[index] = newGraph;
+    this.notifyListeners();
   }
 
   public removeGraph(id: Graph["id"]): void {
     const index = this._graphs.findIndex(g => g.id === id);
     if (index === -1) return;
     this._graphs.splice(index, 1);
+    this.notifyListeners();
   }
 }
