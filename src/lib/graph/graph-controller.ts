2b5dc72296ff756ec36a54b8a124891e8ffa646d
web-liuyang <984584014@qq.com>
2024-05-01 17:28:13 +0800

    Improve: Graph Controller

--- src/lib/graph/graph-controller.ts
+++ src/lib/graph/graph-controller.ts
@@ -1,4 +1,4 @@
-import type { Graph, GraphId } from "../graph";
+import type { Graph, GraphId } from ".";
 import { ChangeNotifier } from "../notifier";
 
 export class GraphController extends ChangeNotifier {
@@ -6,6 +6,10 @@ export class GraphController extends ChangeNotifier {
 
   public get graphs(): Graph[] {
     return [...this._graphs];
+  }
+
+  public get selectedGraphs(): Graph[] {
+    return this._graphs.filter(g => g.selected);
   }
 
   public addGraph(graph: Graph): void {
@@ -17,6 +21,10 @@ export class GraphController extends ChangeNotifier {
     return this._graphs.find(g => g.id === id) as T | undefined;
   }
 
+  public findIndexGraph(id: GraphId): number {
+    return this._graphs.findIndex(g => g.id === id);
+  }
+
   public updateGraph(id: GraphId, newGraph: Graph): void {
     const index = this._graphs.findIndex(g => g.id === id);
     if (index === -1) return;
@@ -24,8 +32,19 @@ export class GraphController extends ChangeNotifier {
     this.notifyListeners();
   }
 
+  public updateGraphs(graphs: Graph[]): void {
+    for (let i = 0, len = graphs.length; i < len; i++) {
+      const graph = graphs[i];
+      const index = this.findIndexGraph(graph.id);
+      if (index === -1) continue;
+      this._graphs[index] = graph;
+    }
+
+    this.notifyListeners();
+  }
+
   public removeGraph(id: GraphId): void {
-    const index = this._graphs.findIndex(g => g.id === id);
+    const index = this.findIndexGraph(id);
     if (index === -1) return;
     this._graphs.splice(index, 1);
     this.notifyListeners();
